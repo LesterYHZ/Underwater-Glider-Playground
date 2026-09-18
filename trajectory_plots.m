5e4d112911c843ac86cd6b47634e9e93f0caae07
LesterYHZ <lester842634640@gmail.com>
2026-09-18 03:27:23 -0400

    UG tutorial

--- trajectory_plots.m
+++ trajectory_plots.m
@@ -3,18 +3,23 @@ clear; clc; close all;
 %% run simulation 
 out = sim('UG_model.slx');
 
+
 %% extract data
 time = out.tout; time = transpose(time);
 eta = out.eta; eta = reshape(eta,6,length(time));
 nu = out.nu; nu = reshape(nu,6,length(time));
 U = out.U; U = transpose(U);
 
+
 %% plot trajectory
 figure('Name','Trajectory');
+
 subplot(2,2,1)
 plot3(eta(1,:),eta(2,:),eta(3,:),'LineWidth',1.5);
 xlabel('x [m]'); ylabel('y [m]'); zlabel('z [m]');
 set(gca,'Zdir','reverse'); grid on;
+set(gca,"FontSize",12);
+
 subplot(2,2,2) 
 yyaxis left
 plot(eta(1,:),eta(2,:),'LineWidth',1.5);
@@ -23,18 +28,24 @@ yyaxis right
 plot(eta(1,:),eta(3,:),'LineWidth',1.5);
 ylabel('z [m]'); 
 set(gca,'Ydir','reverse'); grid on; axis equal;
+set(gca,"FontSize",12);
+
 subplot(2,2,3)
 plot(time,eta(1,:),'LineWidth',1.5); hold on;
 plot(time,eta(2,:),'LineWidth',1.5); 
 plot(time,eta(3,:),'LineWidth',1.5);
 legend('x','y','z');
 xlabel('time [s]'); ylabel('position [m]'); grid on;
+set(gca,"FontSize",12);
+
 subplot(2,2,4)
 plot(time,eta(4,:),'LineWidth',1.5); hold on;
 plot(time,eta(5,:),'LineWidth',1.5);
 plot(time,eta(6,:),'LineWidth',1.5);
 legend('\phi','\theta','\psi');
 xlabel('time [s]'); ylabel('angle [rad]'); grid on;
+set(gca,"FontSize",12);
+
 
 %% plot velocity
 figure('Name','Velocity');
@@ -49,16 +60,24 @@ plot(time,nu(5,:),'LineWidth',1.5);
 plot(time,nu(6,:),'LineWidth',1.5);
 legend('u','v','w','p','q','r');
 xlabel('time [s]'); ylabel('angular velocity [rad/s]'); grid on;
+set(gca,"FontSize",12);
+
 
 %% plot control effort
 figure('Name','Control Effort');
+
 subplot(3,1,1)
 plot(time,U(1,:),'LineWidth',1.5); 
 ylabel('m_b [kg]'); grid on;
+set(gca,"FontSize",12);
+
 subplot(3,1,2)
 plot(time,U(2,:),'LineWidth',1.5);
-ylabel('r_{p1} [m]'); grid on;
+ylabel('r_{p} [m]'); grid on;
+set(gca,"FontSize",12);
+
 subplot(3,1,3)
 plot(time,U(3,:),'LineWidth',1.5);
 ylabel('\gamma [rad]'); grid on;
 xlabel('time [s]');
+set(gca,"FontSize",12);
